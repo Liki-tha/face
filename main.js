7ed0371d341558d5e3335a984788d8211f596020
Liki-tha <71774755+Liki-tha@users.noreply.github.com>
2020-10-22 21:41:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,13 @@
-Webcam.set({
-    width:350,
-    height:300,
-    image_format:"png",
-    png_quality:90,
 
-});
+//console.log("ml5 version :", ml5.version);
+//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ges9y18iU/model.json',modelLoaded);
 
-camera = document.getElementById("camera");
+function preload(){
 
-Webcam.attach('#camera');
-
-function take_snapshot()
-{
-    Webcam.snap(function(data_url)
-    {
-        document.getElementById("result").innerHTML = '<img id = "captured_image" src= "'+data_url+'"/>';
-    });
 }
 
-console.log("ml5 version :", ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ges9y18iU/model.json',modelLoaded);
-
-function modelLoaded()
-{
-    console.log("Model Loaded");
-}
-
-function check()
-{
-    img = document.getElementById("captured_image");
-    classifier.classify(img , gotResult);
-}
-
-function gotResult(error,results)
-{
- if(error)
-  {
-     console.error(error);
-  } else {
-     console.log(results);
-     document.getElementById("result_object_name").innerHTML = results[0].label;
-     document.getElementById("result_object_Accuracy_name").innerHTML = results[0].confidence.toFixed(3);
-
-   }
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    
 }
